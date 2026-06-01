e0b2069d99a621288a865a597f270dc79181effa
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-06-01 17:29:39 +0000

    OK

---
+++ calculaMedia.js
@@ -0,0 +1,12 @@
+let nota1 = 7.5;
+let nota2 = 8.0;
+let nota3 = 6.0;
+let media = nota1 + nota2 + nota3 / 3;
+
+if (media >= 7.0){
+    console.log("Parabens, você foi Aprovado.");
+}
+
+else if (media == 6.0){
+    console.log("Que pena, Você tera que fazer Se Liga.");
+} else("Que pena, Você Reprovou !");
---
+++ radarVelocidade.js
@@ -0,0 +1,8 @@
+let velocidade = 78;
+
+if (velocidade > 80){
+    console.log("Você foi multado por excesso de velocidade !")
+}
+else if(velocidade <= 80){
+    console.log("Velocidade permitida, Boa viagem!");
+}
