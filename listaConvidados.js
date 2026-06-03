723cddcedeaceebc52fa45f6a19c619ac8ac12b5
kauanhurtiak15 <kauan.hurtiak.santos@escola.pr.gov.br>
2026-06-03 13:03:05 +0000

    OK

---
+++ listaConvidados.js
@@ -0,0 +1,9 @@
+let convidados = ["Mailon", "Renan", "Matheus", "Fernando"];
+
+let nome = "Mailon";
+
+if (convidados.includes(nome)){
+    console.log("Pode entrar, você é convidado VIP!")
+} else {
+    console.log("Que pena, você não está na lista.")
+}
---
+++ sistemaLogin.js
@@ -0,0 +1,10 @@
+let usuarioCadastrado = "Admin";
+let senhaCadastrada = "1512";
+let usuarioDigitado = "Admin";
+let senhaDigitada = "0406";
+
+if (usuarioDigitado == usuarioCadastrado && senhaDigitada == senhaCadastrada){
+    console.log("Login efetuado com sucesso!")
+} else {
+    console.log("Usuário ou senha incorretos.")
+}
